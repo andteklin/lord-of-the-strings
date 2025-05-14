8bced17d98200daf534e932f6f94e8ce3f944101
Andrew Lin <smartmonkey3042@gmail.com>
2025-05-14 01:20:25 -0400

    buttons on port A, interrupts

--- main.c
+++ main.c
@@ -4,19 +4,6 @@
 #include "pwm.h"
 #include "MKL46Z4.h"
 #include "strummer.h"
-// #include "strummer.h"
-// #include "systick.h" // or wherever you implement millis
-
-// int main(void) {
-//     init_systick();     // Sets up global millis using SysTick
-//     strummer_init();    // Initializes strummer (servo at rest)
-//     strummer_enable(true); // Start strumming
-
-//     while (1) {
-//         strummer_update(); // non-blocking!
-//         // ... other tasks
-//     }
-// }
 
 int main(void){
     strummer_init();
@@ -27,4 +14,3 @@ int main(void){
     }
     return 0;
 }
-volatile static int i = 1; // counter in memory
